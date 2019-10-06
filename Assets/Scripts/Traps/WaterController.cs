aff8397272f3a4f158af5d0b3869caaf4cb3fffb
Farid Abdalla <farid.abdalla.13@gmail.com>
2019-10-06 22:49:27 +0200

    Water added

---
+++ Assets/Scripts/Traps/WaterController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterController : MonoBehaviour
+{
+    private void Awake()
+    {
+        //if () // player water perks
+        {
+            Physics2D.IgnoreCollision(GetComponent<BoxCollider2D>(), GameObject.FindGameObjectWithTag("Player").GetComponent<CircleCollider2D>());
+        }
+    }
+
+}
