6869e79be093b6e70b3670b4f63c01a4d29f9640
Alexander Wohlfahrt <akawohlfahrt@gmail.com>
2019-10-06 20:30:10 +0200

    Add player characteristics static class

---
+++ Assets/Scripts/PlayerCharacteristics.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PlayerCharacteristics
+{
+    public static bool FireBall { get; set; }
+    public static bool FireBallUp1 { get; set; }
+    public static bool IceBall { get; set; }
+    public static bool IceBallUp1 { get; set; }
+    public static bool RockShield { get; set; }
+    public static bool RockShieldUp1 { get; set; }
+    public static bool AirShield { get; set; }
+    public static bool AirShieldUp1 { get; set; }
+    public static bool PrimMovement { get; set; }
+    public static bool PrimNocVision { get; set; }
+    public static bool PrimLifebarVision { get; set; }
+    public static bool MaxHealth { get; set; }
+    public static bool MaxHealthUp1 { get; set; }
+    public static bool AttackSpeed { get; set; }
+    public static bool AttackSpeedUp1 { get; set; }
+    public static bool AttackDamage { get; set; }
+    public static bool AttackDamageUp1 { get; set; }
+    public static bool SpeedMovement { get; set; }
+    public static bool SpeedMovementUp1 { get; set; }
+    public static bool LifeSteal { get; set; }
+
+    public static void ResetAll()
+    {
+        FireBall = false;
+        FireBallUp1 = false;
+        IceBall = false;
+        IceBallUp1 = false;
+        RockShield = false;
+        RockShieldUp1 = false;
+        AirShield = false;
+        AirShieldUp1 = false;
+        PrimMovement = false;
+        PrimNocVision = false;
+        PrimLifebarVision = false;
+        MaxHealth = false;
+        MaxHealthUp1 = false;
+        AttackSpeed = false;
+        AttackSpeedUp1 = false;
+        AttackDamage = false;
+        AttackDamageUp1 = false;
+        SpeedMovement = false;
+        SpeedMovementUp1 = false;
+        LifeSteal = false;
+    }
+}
