54754665be75013325dd3fe7124472ae158f36ec
Farid Abdalla <farid.abdalla.13@gmail.com>
2019-10-07 02:12:16 +0200

    Add turret

---
+++ Assets/Scripts/Traps/TurretController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretController : MonoBehaviour
+{
+
+    [SerializeField] private GameObject fireballModel;
+    [SerializeField] private List<Transform> fireballOrigins;
+
+
+    private void Awake()
+    {
+        StartCoroutine(Actions());
+    }
+
+    IEnumerator Actions()
+    {
+        yield return new WaitForSeconds(GameParameters.FINAL_BOSS_MOVEMENT_TIME);
+
+        Attack();
+
+        StartCoroutine(Actions());
+    }
+
+    private void Attack()
+    {
+        FireballAttack();
+    }
+
+    private void FireballAttack()
+    {
+        foreach(Transform origin in fireballOrigins)
+        {
+            // Generate fireball
+            GameObject fireball = Instantiate(fireballModel);
+            fireball.transform.parent = transform;
+            fireball.transform.position = origin.position;
+
+            ProjectileController projectileController = fireball.GetComponent<ProjectileController>();
+            projectileController.Shoot(origin.right, gameObject.tag, GameParameters.finalBossFireballSpeed, GameParameters.finalBossFireballDamage);
+
+            CircleCollider2D fireballCollider = fireball.GetComponent<CircleCollider2D>();
+            // Ignore collision between the player and the fireball (trigger ok)
+            Physics2D.IgnoreCollision(GetComponent<BoxCollider2D>(), fireballCollider);
+        }
+        
+    }
+
+}
